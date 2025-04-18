313e2d28af239f33718a9b25a8b6dbc1a5f75a4d
Николай Шестаков <workjustghostw1ng@gmail.com>
2025-04-18 23:44:09 +0300

    create sprite atlases, compress some textures, create level changer

---
+++ Assets/Scripts/LevelChangeButton.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelChangeButton : MonoBehaviour
+{
+	private Button _button;
+
+	private void Awake()
+	{
+		_button = GetComponent<Button>();
+	}
+
+	private void OnEnable()
+	{
+		_button.onClick.AddListener(LoadNextLevel);
+	}
+
+	private void OnDisable()
+	{
+		_button.onClick.RemoveListener(LoadNextLevel);
+	}
+
+	private void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
