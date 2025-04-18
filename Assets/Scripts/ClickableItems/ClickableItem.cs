1ac87f465a57701a8f2ac0c0d174b9e4a2e1ac98
Николай Шестаков <workjustghostw1ng@gmail.com>
2025-04-19 01:02:55 +0300

    add cursor change when detect clickable item

--- Assets/Scripts/ClickableItems/ClickableItem.cs
+++ Assets/Scripts/ClickableItems/ClickableItem.cs
@@ -8,14 +8,29 @@ public class ClickableItem : MonoBehaviour
 	[SerializeField] private int _minPitch = 1;
 	[SerializeField] private int _maxPitch = 4;
 	[SerializeField] private ParticleSystem _particles;
+	[SerializeField] private Texture2D _cursorHover;
 
+	private Vector2 _hotspot;
+	private Texture2D _defaultCursor;
 	private AudioSource _audioSource;
 	private Animator _animator;
 
 	private void Start()
 	{
+		_defaultCursor = null;
+		_hotspot = Vector2.zero;
 		_audioSource = GetComponent<AudioSource>();
 		_animator = GetComponent<Animator>();
+	}
+
+	private void OnMouseEnter()
+	{
+		Cursor.SetCursor(_cursorHover, _hotspot, CursorMode.Auto);
+	}
+
+	private void OnMouseExit()
+	{
+		Cursor.SetCursor(_defaultCursor, Vector2.zero, CursorMode.Auto);
 	}
 
 	private void OnMouseDown()
